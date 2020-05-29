061c99f3a6eaa868e2671ff64494b5564e5ee190
ngoduydat <datngoduy000@gmail.com>
2020-05-29 21:17:50 +0700

    add redux

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,34 @@
 import React, { Fragment } from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Navbar from './components/layout/Navbar';
 import Landing from './components/layout/Landing';
+import Alert from './components/layout/Alert';
+import Login from './components/auth/Login';
+import Register from './components/auth/Register';
+
+// Redux
+import { Provider } from 'react-redux';
+import store from './store';
+
 import './App.css';
 
 const App = () => (
-	<Fragment>
-		<Navbar />
-		<Landing />
-	</Fragment>
+	<Provider store={store}>
+		<Router>
+			<Fragment>
+				<Navbar />
+
+				<Route exact path='/' component={Landing} />
+				<section className='container'>
+					<Alert />
+					<Switch>
+						<Route exact path='/login' component={Login} />
+						<Route exact path='/Register' component={Register} />
+					</Switch>
+				</section>
+			</Fragment>
+		</Router>
+	</Provider>
 );
 
 export default App;
